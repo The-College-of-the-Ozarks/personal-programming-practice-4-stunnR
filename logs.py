f2178cb870cfc94ea71a22659c39ea38a33628d3
stunnR <112041449+stunnR@users.noreply.github.com>
2022-09-14 14:15:53 -0500

    Wrote code

--- logs.py
+++ logs.py
@@ -7,7 +7,17 @@ import math
 def g(x):
     return math.log(100 - x**2) + math.sqrt(84 - 5*x - x**2)
 
-x = input("Input the value for x: ")
-x = float(x)
+x = input("Input a value for x larger than -10 and less than or equal to 7: ")
 
-print("g(" + str(x) + ") = " + str(g(x)))
+# exits program if user enters anything but an number
+try:
+    x = float(x)
+except:
+    print("Invalid input. x must be a number.")
+    exit()
+
+# if x is within function domain, computes function; else exits program
+if (x <= -10) or (x > 7):
+    print("Invalid input. x must be larger than -10 and less than or equal to 7.")
+else:
+    print("g(" + str(x) + ") = " + str(g(x)))
